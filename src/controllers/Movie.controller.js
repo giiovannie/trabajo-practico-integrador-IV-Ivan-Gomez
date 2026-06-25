88a4293f9fb233d6cc78bb1a55fa05de8e88c61e
ivan Emmanuel Gomez <ivanemmanuelgomez2017@gmail.com>
2026-06-25 09:36:19 -0300

    feat: se agrego un endpoint con un metodo post

--- src/controllers/Movie.controller.js
+++ src/controllers/Movie.controller.js
@@ -30,3 +30,47 @@ export const findMovie = async (req,res)=>{
         res.styatus(400).json(errorMessage)
     }
 }
+
+export const insertMovie = async (req,res)=>{
+    try {
+        const {title, genere, duration, year,synopsis } = req.body;
+
+        if(!title || !genre || !duration || !year){
+            return res.status(400).json(errorMessage)
+        }
+        
+        if(!Number.isInteger(Number(duration)) || Number(duration) <= 0){
+            return res.status(400).json(errorMessage)
+        }
+        
+        if(
+            !Number.isInteger(Number(year)) || 
+            Number(year) < 1888
+        ){
+            return res.status(400).json(errorMessage)
+        }
+
+        const exists = await TableMovie.findOne({
+            where: { title }
+        });
+
+        if (exists) {
+            return res.status(400).json({
+                message: "Ya existe una película con ese título"
+            });
+        }
+
+        const agregarPelicula = await TableMovie.create({
+            title,
+            genere,
+            duration,
+            year,
+            synopsis
+        })
+
+        res.json(agregarPelicula)
+
+    } catch (error) {
+        res.status(401).json(errorMessage)
+    }
+}
